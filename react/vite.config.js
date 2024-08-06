c3a57dc85cfe1126e64d0daa2d2b180619bd7081
ja166667167 <ja166667167@gmail.com>
2024-08-06 18:20:14 +0800

    fix issue and deploy prod

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,18 +1,37 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "build",
-  },
-  // server: {
-  //   watch: {
-  //     usePolling: true,
-  //   },
-  //   host: true, // needed for the Docker Container port mapping to work
-  //   strictPort: true,
-  //   port: 5173, // you can replace this port with any port
-  // },
-});
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "build",
+//   },
+//   server: {
+//     watch: {
+//       usePolling: true,
+//     },
+//     host: true, // needed for the Docker Container port mapping to work
+//     strictPort: true,
+//     port: import.meta.env.VITE_REACT_PORT,
+//     // port: 81,
+//   },
+// });
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return defineConfig({
+    plugins: [react()],
+    build: {
+      outDir: "build",
+    },
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true, // needed for the Docker Container port mapping to work
+      strictPort: true,
+      port: env.VITE_REACT_PORT,
+      // port: 81,
+    },
+  });
+};
